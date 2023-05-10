2d2cf368a0b83601ddd04332474c20baa6e53615
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-05-10 14:52:49 +0530

    Create Hackerrank-Queen-Attack-II-Successful-Python.py

---
+++ Hackerrank-Queen-Attack-II-Successful-Python.py
@@ -0,0 +1,28 @@
+#Successful Variant (Learnt and implemented from the Hackerrank Solution on Programmingoneonone)
+moves = {'n':(n - r_q), 's':(r_q - 1), 'e':(n - c_q), 'w':(c_q - 1), 'ne':min(n - r_q, n - c_q), 
+         'nw':min(n - r_q, c_q - 1), 'se':min(r_q - 1, n - c_q), 'sw':min(r_q - 1, c_q - 1)}
+
+for i in range(k):
+    r, c = map(int, input().split())
+    if c == c_q:
+        if r > r_q:
+            moves['n'] = min(r - r_q - 1, moves['n'])
+        else:
+            moves['s'] = min(r_q - r - 1, moves['s'])
+    elif r == r_q:
+        if c > c_q:
+            moves['e'] = min(c - c_q - 1, moves['e'])
+        else:
+            moves['w'] = min(c_q - c - 1, moves['w'])
+    elif r - c == r_q - c_q:
+        if r > r_q and c > c_q:
+            moves['ne'] = min(r - r_q - 1, moves['ne'])
+        elif r < r_q and c < c_q:
+            moves['sw'] = min(r_q - r - 1, moves['sw'])
+    elif r + c == r_q + c_q:
+        if r > r_q and c < c_q:
+            moves['nw'] = min(r - r_q - 1, moves['nw'])
+        elif r < r_q and c > c_q:
+            moves['se'] = min(r_q - r - 1, moves['se'])
+        
+print (sum(moves.values()))
