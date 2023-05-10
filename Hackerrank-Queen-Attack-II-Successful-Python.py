c2b269a89bdb5c667ba319ba359533ba41392e46
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-05-10 15:36:50 +0530

    Update Hackerrank-Queen-Attack-II-Successful-Python.py

--- Hackerrank-Queen-Attack-II-Successful-Python.py
+++ Hackerrank-Queen-Attack-II-Successful-Python.py
@@ -1,9 +1,13 @@
 #Successful Variant (Learnt and implemented from the Hackerrank Solution on Programmingoneonone)
 
-#calculating the maximum number of steps that can be taken by the queen
+n, k = map(int, input().split())
+r_q, c_q = map(int, input(). split())
+
+#creating a dictionary and calculating the maximum number of steps that can be taken by the queen in a given direction
 moves = {'n':(n - r_q), 's':(r_q - 1), 'e':(n - c_q), 'w':(c_q - 1), 'ne':min(n - r_q, n - c_q), 
          'nw':min(n - r_q, c_q - 1), 'se':min(r_q - 1, n - c_q), 'sw':min(r_q - 1, c_q - 1)}
 
+#taking input of the position of obstacles and comparing if the obstacles lies in path of queen, if yes, calculate steps
 for i in range(k):
     r, c = map(int, input().split())
     if c == c_q:
@@ -26,5 +30,6 @@ for i in range(k):
             moves['nw'] = min(r - r_q - 1, moves['nw'])
         elif r < r_q and c > c_q:
             moves['se'] = min(r_q - r - 1, moves['se'])
-        
+
+#calculating the total moves of the 
 print (sum(moves.values()))
