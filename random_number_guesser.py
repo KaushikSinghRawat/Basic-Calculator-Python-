b76697a072dca8ec05d98c89ac499679f7614679
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-07-11 20:31:12 +0530

    Add files via upload

---
+++ random_number_guesser.py
@@ -0,0 +1,21 @@
+import random
+
+top_range = int(input("Enter the top range: ").strip())
+bottom_range = int(input("Enter the bottom range: ").strip())
+
+rand_generator = random.randrange(bottom_range, top_range + 1)
+
+checker = False
+
+while checker == False:
+    answer = int(input("Enter your guess: ").strip())
+
+    if rand_generator == answer:
+        print (f"Correct, the Random Number was {rand_generator}")
+        checker = True
+    else:
+        if rand_generator < answer:
+            print ("The random number is lower")
+        else: 
+            print ("The random number is higher")
+
